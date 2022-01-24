4fcae3e3ebe5650c9f2770ac6d97369b8d48a018
zijadlicina <zijadlicina1@gmail.com>
2022-01-24 14:16:37 +0100

    Refactor callback to promise

--- index.js
+++ index.js
@@ -1,25 +1,37 @@
 
-// First reslove of this issue: is to call callback function, pass like a parametar
-// Callback function -> that's gonna run later
+// If we want to send some callback functions on 'onFailure' stage
+// our code will be really complicated
+
+// Because of this we are using "Promise":
 console.log('Start');
 
-function loginUser(email, password, callback){
-    setTimeout(() => {
-        console.log("Now we have the date");
-        callback ({userEmail: email})
-    }, 3000);
+function loginUser(email, password){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("Now we have the date");
+            resolve ({userEmail: email})
+        }, 3000);
+    })
 }
-function getUserVideos(userEmail, callback){
-    setTimeout(() => {
-        callback (["video1", "video2", "video3"])
-    }, 2000);
+function getUserVideos(userEmail){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve (["video1", "video2", "video3"])
+        }, 2000);
+    })
 }
-
+/*
 const user = loginUser("user1@gmail.com", 123456, (user) => {
     console.log(user);
     getUserVideos(user.userEmail, (videos)=>{
         console.log(videos)
     })
 }); 
+*/
+
+loginUser("user1", 123)
+    .then(user => getUserVideos(user.userEmail))
+    .then(videos => console.log(videos))
+
 console.log("End");
 
