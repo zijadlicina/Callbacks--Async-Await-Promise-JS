75be05f48fcc5d8e3ef17e38af118df7278a7a5e
zijadlicina <zijadlicina1@gmail.com>
2022-01-24 12:11:14 +0100

    Sync code

---
+++ index.js
@@ -0,0 +1,14 @@
+// Sync code example
+
+function otherFunction(){
+    console.log("we are in other function");
+    console.log("do some stuff");
+}
+
+console.log('Start');
+
+otherFunction();
+
+console.log("End");
+
+// JS read code line by line
