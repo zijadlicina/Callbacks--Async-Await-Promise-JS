aef7123c8dce510cf8dad59dca45e91663ecbe83
zijadlicina <zijadlicina1@gmail.com>
2022-01-24 12:22:27 +0100

    Async code

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-// Sync code example
+// Async code example
 
-function otherFunction(){
-    console.log("we are in other function");
-    console.log("do some stuff");
-}
 
 console.log('Start');
 
-otherFunction();
+setTimeout(()=>{
+    console.log("we are in other function");
+    console.log("do some stuff");
+}, 2000);
 
 console.log("End");
 
-// JS read code line by line
+// JS read code line by line/ Single threaded language
+/* In this situation after 5s our code in "SetTimeout" will log out
+    -> our "Callstack" is send this function to "Web Apis stack"
+    (also "eventListener" function are being in this stack) */
