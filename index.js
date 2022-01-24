78b7202ebcf884fa6279b76f745d9ac587202560
zijadlicina <zijadlicina1@gmail.com>
2022-01-24 13:47:39 +0100

    Sending callback like a parametar

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 
+// First reslove of this issue: is to call callback function, pass like a parametar
+// Callback function -> that's gonna run later
 console.log('Start');
 
 // Example of some server who returns some data
-function loginUser(email, password){
+function loginUser(email, password, callback){
     setTimeout(() => {
         console.log("Now we have the date");
-        return {userEmail: email}
+        callback ({userEmail: email})
     }, 3000);
 }
 
-const user = loginUser("user1@gmail.com", 123456);
-console.log(user);  // user give us 'undefined'
-
+const user = loginUser("user1@gmail.com", 123456, (user) => {
+    console.log(user);
+}); 
 console.log("End");
 
