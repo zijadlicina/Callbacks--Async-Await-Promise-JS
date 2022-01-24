6098411cf69caf89e206cf718acbdf7a512acede
zijadlicina <zijadlicina1@gmail.com>
2022-01-24 12:29:59 +0100

    Callback function

--- index.js
+++ index.js
@@ -2,15 +2,20 @@
 
 
 console.log('Start');
+/*
+callback function - when data are avalilable send this function in callback to "Callstack"
+() => {
 
-setTimeout(()=>{
-    console.log("we are in other function");
-    console.log("do some stuff");
-}, 2000);
+}
+*/
+
+let items = [1, 2, 3, 4, 5];
+
+// fore_each takes callbacks function
+items.forEach((item) => {
+    console.log(item);
+})
 
 console.log("End");
 
-// JS read code line by line/ Single threaded language
-/* In this situation after 5s our code in "SetTimeout" will log out
-    -> our "Callstack" is send this function to "Web Apis stack"
-    (also "eventListener" function are being in this stack) */
+// Callbacks are not necessary async, they can be sync too
