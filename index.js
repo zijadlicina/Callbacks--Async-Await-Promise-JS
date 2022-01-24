858589f603eadda9592a1a1e9ae2623fda3f2c3a
zijadlicina <zijadlicina1@gmail.com>
2022-01-24 13:54:22 +0100

    Callback second

--- index.js
+++ index.js
@@ -3,16 +3,23 @@
 // Callback function -> that's gonna run later
 console.log('Start');
 
-// Example of some server who returns some data
 function loginUser(email, password, callback){
     setTimeout(() => {
         console.log("Now we have the date");
         callback ({userEmail: email})
     }, 3000);
 }
+function getUserVideos(userEmail, callback){
+    setTimeout(() => {
+        callback (["video1", "video2", "video3"])
+    }, 2000);
+}
 
 const user = loginUser("user1@gmail.com", 123456, (user) => {
     console.log(user);
+    getUserVideos(user.userEmail, (videos)=>{
+        console.log(videos)
+    })
 }); 
 console.log("End");
 
